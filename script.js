fae2616cdd8890b322f837205e20b2b89bbc4436
BuildTools <unconfigured@null.spigotmc.org>
2023-10-30 14:47:41 +0100

    Shader Update

---
+++ script.js
@@ -0,0 +1,182 @@
+////////////////////////////////////////////////////////////////////////////////
+// BOILERPLATE START
+
+// Get the WebGL context
+const canvas = document.getElementById('canvas')
+const gl = canvas.getContext('webgl2')
+
+// Add mouse move event handlers to the canvas to update the cursor[] array.
+const cursor = [0, 0]
+canvas.addEventListener('mousemove', (event) =>
+{
+    cursor[0] = (event.offsetX / canvas.width) * 2 - 1
+    cursor[1] = (event.offsetY / canvas.height) * -2 + 1
+})
+
+// Basic render loop manager.
+function setRenderLoop(callback)
+{
+    function renderLoop(time)
+    {
+        if (setRenderLoop._callback !== null) {
+            setRenderLoop._callback(time)
+            requestAnimationFrame(renderLoop)
+        }
+    }
+    setRenderLoop._callback = callback
+    requestAnimationFrame(renderLoop)
+}
+setRenderLoop._callback = null
+
+// BOILERPLATE END
+////////////////////////////////////////////////////////////////////////////////
+// Shader //////////////////////////////////////////////////////////////////////
+
+const vertexShaderSource = `#version 300 es
+    precision highp float; // Calculate the varying outputs with high precision
+
+    in vec2 a_pos;
+    in vec3 a_color;
+
+    out vec3 f_color;
+
+    void main() {
+         gl_Position = vec4(a_pos, 0.0, 1.0);
+         f_color = a_color;
+    }
+`
+
+const fragmentShaderSource = `#version 300 es
+    precision mediump float;
+    #endif
+
+    uniform vec2 u_resolution;
+    uniform vec2 u_mouse;
+    uniform float u_time;
+
+    float random (in vec2 st) {
+        return fract(sin(dot(st.xy,
+                            vec2(12.9898,78.233)))*
+            43758.5453123);
+    }
+
+    vec2 random2(vec2 p) {
+        return fract(sin(vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))))*43758.5453);
+    }
+
+    float cellular(vec2 p) {
+        vec2 i_st = floor(p);
+        vec2 f_st = fract(p);
+        float m_dist = 10.;
+        for (int j=-1; j<=1; j++ ) {
+            for (int i=-1; i<=1; i++ ) {
+                vec2 neighbor = vec2(float(i),float(j));
+                vec2 point = random2(i_st + neighbor);
+                point = 0.5 + 0.5*sin(6.2831*point);
+                vec2 diff = neighbor + point - f_st;
+                float dist = length(diff);
+                if( dist < m_dist ) {
+                    m_dist = dist;
+                }
+            }
+        }
+        return m_dist;
+    }
+
+    void main() {
+        vec2 st = gl_FragCoord.xy / u_resolution.xy;
+        st.x *= u_resolution.x / u_resolution.y;
+        st *= 5.0;
+        float r = cellular(st);
+        float b = cellular(st - vec2(0.0, sin(u_time * 0.5) * 0.5));
+        gl_FragColor = vec4(r, 0.0, b, 1.0);
+    }
+`
+
+// Create the Vertex Shader
+const vertexShader = gl.createShader(gl.VERTEX_SHADER)
+gl.shaderSource(vertexShader, vertexShaderSource)
+gl.compileShader(vertexShader)
+
+// Create the Fragment Shader
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+gl.shaderSource(fragmentShader, fragmentShaderSource)
+gl.compileShader(fragmentShader)
+
+// Link the two into a single Shader Program
+const shaderProgram = gl.createProgram()
+gl.attachShader(shaderProgram, vertexShader)
+gl.attachShader(shaderProgram, fragmentShader)
+gl.linkProgram(shaderProgram)
+gl.useProgram(shaderProgram)
+
+// Data ////////////////////////////////////////////////////////////////////////
+
+const vertexPositions = new Float32Array([
+    -1., -1., 1, 0, 0,
+    +1., -1., 0, 1, 0,
+    +1., +1., 0, 0, 1,
+    -1., +1., 1, 1, 1,
+])
+
+// Create the position buffer
+const positionBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW)
+
+const faceIndices = new Uint16Array([
+    0, 1, 2, // first triangle
+    0, 2, 3, // second triangle
+])
+
+// Create the index buffer
+const indexBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, faceIndices, gl.STATIC_DRAW)
+
+// Attribute Mapping ///////////////////////////////////////////////////////////
+
+// Map the contents of the buffer to the vertex shader
+const vertexAttribute = gl.getAttribLocation(shaderProgram, 'a_pos')
+gl.enableVertexAttribArray(vertexAttribute)
+gl.vertexAttribPointer(
+    vertexAttribute,
+    2,        // numComponents
+    gl.FLOAT, // type
+    false,    // normalize
+    20,       // stride
+    0         // offset
+)
+
+const colorAttribute = gl.getAttribLocation(shaderProgram, 'a_color')
+gl.enableVertexAttribArray(colorAttribute)
+gl.vertexAttribPointer(
+    colorAttribute,
+    3,        // numComponents
+    gl.FLOAT, // type
+    false,    // normalize
+    20,       // stride
+    8         // offset
+)
+
+// Uniforms ////////////////////////////////////////////////////////////////////
+
+const timeUniform = gl.getUniformLocation(shaderProgram, "u_time")
+const cursorUniform = gl.getUniformLocation(shaderProgram, "u_cursor")
+
+// Rendering ///////////////////////////////////////////////////////////////////
+
+function renderLoop(time)
+{
+    gl.uniform1f(timeUniform, time / 5000)
+    gl.uniform2f(cursorUniform, cursor[0], cursor[1])
+
+    // Draw the scene.
+    gl.drawElements(
+        gl.TRIANGLES,       // primitive type
+        faceIndices.length, // vertex count
+        gl.UNSIGNED_SHORT,  // type of indices
+        0                   // offset
+    )
+}
+setRenderLoop(renderLoop)
